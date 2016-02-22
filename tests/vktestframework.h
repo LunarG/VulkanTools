3a70f2fc740d359e3c4495b5e84a055b501878eb
Jon Ashburn <jon@lunarg.com>
2016-02-22 11:25:47 -0700

    Revert "Remove all tests except for layer validation and doc validation"
    This reverts commit 55e270ba35534b24f3fd60930f129c18ec9e430f.
    
    Conflicts:
    	tests/CMakeLists.txt
    	tests/vktestframework.cpp
    	tests/vktestframework.h

--- tests/vktestframework.h
+++ tests/vktestframework.h
@@ -65,17 +65,42 @@ using namespace std;
 
 class VkImageObj;
 
-class VkTestFramework : public ::testing::Test {
-  public:
+
+class VkTestImageRecord
+{
+public:
+    VkTestImageRecord();
+    VkTestImageRecord(const VkTestImageRecord &);
+    ~VkTestImageRecord();
+    VkTestImageRecord &operator=(const VkTestImageRecord &rhs);
+    int operator==(const VkTestImageRecord &rhs) const;
+    int operator<(const VkTestImageRecord &rhs) const;
+
+    string                    m_title;
+    int                       m_width;
+    int                       m_height;
+    void                     *m_data;
+    unsigned                  m_data_size;
+};
+
+class VkTestFramework : public ::testing::Test
+{
+public:
     VkTestFramework();
     ~VkTestFramework();
 
-    VkFormat GetFormat(VkInstance instance, vk_testing::Device *device);
-    static bool optionMatch(const char *option, char *optionLine);
+    static bool optionMatch(const char* option, char* optionLine);
     static void InitArgs(int *argc, char *argv[]);
     static void Finish();
 
-    bool GLSLtoSPV(const VkShaderStageFlagBits shader_type, const char *pshader,
+    void WritePPM( const char *basename, VkImageObj *image );
+    void Show(const char *comment, VkImageObj *image);
+    void Compare(const char *comment, VkImageObj *image);
+    void RecordImage(VkImageObj * image);
+    void RecordImages(vector<VkImageObj *> image);
+    VkFormat GetFormat(VkInstance instance, vk_testing::Device *device);
+    bool GLSLtoSPV(const VkShaderStageFlagBits shader_type,
+                   const char *pshader,
                    std::vector<unsigned int> &spv);
     static bool m_use_glsl;
     static bool m_canonicalize_spv;
@@ -94,10 +119,21 @@ class VkTestFramework : public ::testing::Test {
     EShLanguage FindLanguage(const std::string &name);
     EShLanguage FindLanguage(const VkShaderStageFlagBits shader_type);
     std::string ConfigFile;
-    bool SetConfigFile(const std::string &name);
-    static int m_width;
-    static int m_height;
-    string m_testName;
+    bool SetConfigFile(const std::string& name);
+
+    static bool                             m_show_images;
+    static bool                             m_save_images;
+    static bool                             m_compare_images;
+
+    static std::list<VkTestImageRecord>     m_images;
+    static std::list<VkTestImageRecord>::iterator m_display_image;
+    static int                              m_display_image_idx;
+
+    static int                              m_width;            // Window width
+    static int                              m_height;           // Window height
+
+    int                                     m_frameNum;
+    string                                  m_testName;
 };
 
 class TestEnvironment : public ::testing::Environment {
