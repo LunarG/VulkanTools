0a23f70205de1ce20e7ecac7e8ad774a92d637ae
Karl Schultz <karl@lunarg.com>
2016-04-06 19:10:42 -0600

    tests: rework vktrace/replay script.

--- tests/vktracereplay.sh
+++ tests/vktracereplay.sh
@@ -12,40 +12,39 @@ fi
 
 printf "$GREEN[ RUN      ]$NC $0\n"
 
-# Create a temp directory to run the test in
-rm -rf vktracereplay_tmp
-mkdir vktracereplay_tmp
-cd vktracereplay_tmp
-cp ../../vktrace/vkreplay .
-cp ../../vktrace/vktrace .
-cp ../../vktrace/libVkLayer_vktrace_layer.so .
-cp ../../demos/cube .
-cp ../../demos/*ppm .
-cp ../../demos/*spv .
-export LD_LIBRARY_PATH=`pwd`/../loader:$LD_LIBRARY_PATH
-export VK_LAYER_PATH=`pwd`/../../layersvt
+export LD_LIBRARY_PATH=${PWD}/../loader:${LD_LIBRARY_PATH}
+export VK_LAYER_PATH=${PWD}/../layersvt
 
-(
-    ./vktrace -s 1 -p cube  -o c01.vktrace &
-    P=$!
-    sleep 3
-    kill $P
-) >/dev/null 2>&1
-mv 1.ppm 1_trace.ppm
-./vkreplay -s 1 -t c01.vktrace >/dev/null 2>&1
-#cp cube 1.ppm  # For testing this script -- force a failure
-#rm 1.ppm       # For testing this script -- force a failure
-cmp -s 1.ppm 1_trace.ppm
-RES=$?
-cd ..
-rm -rf vktracereplay_tmp
+function trace_replay {
+	PGM=$1
+	VKTRACE=${PWD}/../vktrace/vktrace
+	VKREPLAY=${PWD}/../vktrace/vkreplay
+	APPDIR=${PWD}/../demos
+	printf "$GREEN[ TRACE    ]$NC ${PGM}\n"
+	${VKTRACE}	--Program ${APPDIR}/${PGM} \
+			--Arguments "--c 100" \
+			--WorkingDir ${APPDIR} \
+			--OutputTrace ${PGM}.vktrace \
+			-s 1
+	printf "$GREEN[ REPLAY   ]$NC ${PGM}\n"
+	${VKREPLAY}	--TraceFile ${PGM}.vktrace \
+			-s 1
+	rm -f ${PGM}.vktrace
+	cmp -s 1.ppm ${APPDIR}/1.ppm
+	RES=$?
+	rm 1.ppm ${APPDIR}/1.ppm
+	if [ $RES -eq 0 ] ; then
+	   printf "$GREEN[  PASSED  ]$NC ${PGM}\n"
+	else
+	   printf "$RED[  FAILED  ]$NC screenshot file compare failed\n"
+	   printf "$RED[  FAILED  ]$NC ${PGM}\n"
+	   printf "TEST FAILED\n"
+	   exit 1
+	fi
+}
 
-if [ $RES -eq 0 ] ; then
-   printf "$GREEN[  PASSED  ]$NC 1 test\n"
-   exit 0
-else
-   printf "$RED[  FAILED  ]$NC screenshot file compare failed\n"
-   printf "$RED[  FAILED  ]$NC 1 test\n"
-   printf "1 TEST FAILED\n"
-   exit 1
-fi
+trace_replay cube
+trace_replay tri
+
+exit 0
+
