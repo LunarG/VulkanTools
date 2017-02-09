5914a2e5baa33f3bdd2495ce8c966cefd30144ba
mizhen <ming.zheng@amd.com>
2017-02-09 13:27:25 -0700

    vktrace: trim hotkey feature

--- vktrace/vktrace_layer/vktrace_lib_trace.cpp
+++ vktrace/vktrace_layer/vktrace_lib_trace.cpp
@@ -3142,6 +3142,33 @@ VKTRACER_EXPORT VKAPI_ATTR VkResult VKAPI_CALL __HOOKED_vkGetSwapchainImagesKHR(
     return result;
 }
 
+void trim_Start()
+{
+    g_trimIsPreTrim = false;
+    g_trimIsInTrim = true;
+    trim::snapshot_state_tracker();
+}
+
+void trim_Stop()
+{
+    g_trimIsInTrim = false;
+    g_trimIsPostTrim = true;
+
+    // This will write packets to recreate ONLY THE REFERENCED objects.
+    trim::write_all_referenced_object_calls();
+
+    // write the packets that were recorded during the trim frames
+    trim::write_recorded_packets();
+
+    // write packets to destroy all created objects
+    trim::write_destroy_packets();
+
+    // clean up
+    trim::delete_all_packets();
+
+    g_trimAlreadyFinished = true;
+}
+
 VKTRACER_EXPORT VKAPI_ATTR VkResult VKAPI_CALL __HOOKED_vkQueuePresentKHR(
     VkQueue queue,
     const VkPresentInfoKHR* pPresentInfo)
@@ -3334,6 +3361,7 @@ VKTRACER_EXPORT VKAPI_ATTR VkResult VKAPI_CALL __HOOKED_vkCreateXcbSurfaceKHR(
     VkResult result;
     vktrace_trace_packet_header* pHeader;
     packet_vkCreateXcbSurfaceKHR* pPacket = NULL;
+    trim::set_keyboard_connection(pCreateInfo->connection);
     // don't bother with copying the actual xcb window and connection into the trace packet, vkreplay has to use it's own anyway
     CREATE_TRACE_PACKET(vkCreateXcbSurfaceKHR, sizeof(VkSurfaceKHR) + sizeof(VkAllocationCallbacks) + sizeof(VkXcbSurfaceCreateInfoKHR));
     result = mid(instance)->instTable.CreateXcbSurfaceKHR(instance, pCreateInfo, pAllocator, pSurface);
