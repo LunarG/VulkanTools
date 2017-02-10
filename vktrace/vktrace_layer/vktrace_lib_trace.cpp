661835c32a856fd3d3c999e8939b2e94e3ea9bb6
Jeffrey Yang <jeffrey.yang@arm.com>
2017-02-10 15:03:54 +0800

    vktrace: Fix a 4 bytes alignment issue
    Both __HOOKED_vkFlushMappedMemoryRanges() and
    vkFlushMappedMemoryRangesWithoutAPICall call
    getALLChangedPackageSizeInMappedMemory(), but
    only __HOOKED_vkFlushMappedMemoryRange rounds
    the size to 4 bytes alignment.
    
    Move the ROUNDUP_TO_4() into the function to
    avoid calling it in two places.

--- vktrace/vktrace_layer/vktrace_lib_trace.cpp
+++ vktrace/vktrace_layer/vktrace_lib_trace.cpp
@@ -752,7 +752,7 @@ VKTRACER_EXPORT VKAPI_ATTR VkResult VKAPI_CALL __HOOKED_vkFlushMappedMemoryRange
         dataSize += ROUNDUP_TO_4(((size_t)(getPageGuardControlInstance().getMappedMemorySize(device, pRange->memory));
     }
 #else
-    dataSize = ROUNDUP_TO_4(getPageGuardControlInstance().getALLChangedPackageSizeInMappedMemory(device, memoryRangeCount, pMemoryRanges, ppPackageData));
+    dataSize = getPageGuardControlInstance().getALLChangedPackageSizeInMappedMemory(device, memoryRangeCount, pMemoryRanges, ppPackageData);
 #endif
     rangesSize = sizeof(VkMappedMemoryRange) * memoryRangeCount;
 
