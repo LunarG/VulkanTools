e98bb3d5205de004a006bc622631d5b28e764680
mizhen <ming.zheng@amd.com>
2017-02-14 10:33:16 -0500

    vktrace: add description for env var
    Add description for env var _VKTRACE_OPTIMIZE_PMB, VKTRACE_PAGEGUARDTARGETSIZE and VKTRACE_PAGEGUARD_ENABLE_READ_PMB.
    
    XCAP-507
    
    Change-Id: I88f2910eb1e496efa7a5fee9109719b3802f0351

--- vktrace/vktrace_layer/vktrace_lib_pageguard.h
+++ vktrace/vktrace_layer/vktrace_lib_pageguard.h
@@ -65,12 +65,23 @@
 
 #include "vktrace_pageguard_memorycopy.h"
 
-
+//_VKTRACE_OPTIMIZE_PMB env var enable PMB support if the value is 1, other
+// value disable PMB support, if this env var is undefined,a default value
+// will be used;
 #define PAGEGUARD_PAGEGUARD_ENABLE_ENV "_VKTRACE_OPTIMIZE_PMB"
+// VKTRACE_PAGEGUARDTARGETSIZE env var specify the mapped size of memory
+// objects managed by page guard, vktrace only add page guard for memory
+// object of which mapped memory size > this value. If this env var is
+// not defined, page guard manages all mapped memory, this is the default
+// option. There’s no need for user to specify this env variable except
+// for debug.
 #define PAGEGUARD_PAGEGUARD_TARGET_RANGE_SIZE_ENV "VKTRACE_PAGEGUARDTARGETSIZE"
 
 // VKTRACE_PAGEGUARD_ENABLE_READ_PMB env var enables read PMB support. Only
-// supported on Windows. Not yet tested.
+// supported on Windows. Some PMB data change come from GPU side and cannot
+// be captured by writes page guard, such data change may affect target
+// application running if target application read it. the env var is used
+// to enable capture such PMB data change.
 #define PAGEGUARD_PAGEGUARD_ENABLE_READ_PMB_ENV "VKTRACE_PAGEGUARD_ENABLE_READ_PMB"
 // PAGEGUARD_PAGEGUARD_ENABLE_READ_PMB_POST_PROCESS_ENV env var enables post process for read PMB support. Only
 // supported on Windows. page guard process miss following write access if read access happen on same page for some titles which
