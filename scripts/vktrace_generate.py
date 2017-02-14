acdd6bb40600feec0a3905658490bd0d62e784a3
Peter Lohrmann <plohrmann@gmail.com>
2017-02-13 19:50:46 -0800

    vktrace trim: Fix tracking of SurfaceKHR object destruction.
    XCAP-536
    
    Change-Id: I221ee5b7e98a91faaa20cbd5efd9b50c0c84b94c

--- scripts/vktrace_generate.py
+++ scripts/vktrace_generate.py
@@ -1134,6 +1134,16 @@ memoryTypeBits=%0x08X}",
             trim_instructions.append('        {')
             trim_instructions.append('            vktrace_delete_trace_packet(&pHeader);')
             trim_instructions.append('        }')
+        elif 'DestroySurfaceKHR' is proto.name:
+            trim_instructions.append("        trim::remove_SurfaceKHR_object(surface);")
+            trim_instructions.append('        if (g_trimIsInTrim)')
+            trim_instructions.append('        {')
+            trim_instructions.append('            trim::add_recorded_packet(pHeader);')
+            trim_instructions.append('        }')
+            trim_instructions.append('        else')
+            trim_instructions.append('        {')
+            trim_instructions.append('            vktrace_delete_trace_packet(&pHeader);')
+            trim_instructions.append('        }')
         elif 'DestroyRenderPass' is proto.name:
             trim_instructions.append("        trim::remove_RenderPass_object(renderPass);")
             trim_instructions.append('        if (g_trimIsInTrim)')
