3093dbe5a85ba84dcbb8ddcedf6b31dd8350c960
Mark Lobodzinski <mark@lunarg.com>
2017-05-08 10:20:42 -0600

    build: Switch to xml-codegen for vktrace_vk_vk.cpp
    Change-Id: Ia6e5f45f052d4c6f5143dde2437722962814d44f

--- build-android/android-generate.bat
+++ build-android/android-generate.bat
@@ -36,7 +36,7 @@ py -3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml api_dump_t
 
 REM vktrace
 py -3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_vk.h
-py -3 ../../../scripts/vktrace_generate.py AllPlatforms vktrace-trace-c vk_version_1_0 > vktrace_vk_vk.cpp
+py -3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_vk.cpp
 py -3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_vk_packets.h
 py -3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_packet_id.h
 
--- build-android/android-generate.sh
+++ build-android/android-generate.sh
@@ -40,7 +40,7 @@ mkdir -p generated/include generated/common
 
 # vktrace
 ( cd generated/include; python3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_vk.h)
-python3 ../scripts/vktrace_generate.py AllPlatforms vktrace-trace-c vk_version_1_0 > generated/include/vktrace_vk_vk.cpp
+( cd generated/include; python3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_vk.cpp)
 ( cd generated/include; python3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_vk_packets.h)
 ( cd generated/include; python3 ../../../scripts/lvl_genvk.py -registry ../../../scripts/vk.xml vktrace_vk_packet_id.h)
 
