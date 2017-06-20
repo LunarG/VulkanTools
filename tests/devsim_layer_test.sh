1eeb25a9f220cd3586a01941d8fa07b5de3c2a00
Mike Weiblen <mikew@lunarg.com>
2017-06-19 19:06:43 -0600

    tests: Create device simulation layer tests
    Add test datafile, Linux test runner script, and adjust CMakeLists
    to copy to build directory.
    
    Change-Id: Iad49f7afba9216b900d3a930e5fb72082e6c7958

---
+++ tests/devsim_layer_test.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+# simple test of devsim layer
+
+set errexit
+set nounset
+set physical
+
+cd $(dirname "${BASH_SOURCE[0]}")
+
+if [ -t 1 ] ; then
+    RED='\033[0;31m'
+    GREEN='\033[0;32m'
+    NC='\033[0m' # No Color
+else
+    RED=''
+    GREEN=''
+    NC=''
+fi
+
+printf "$GREEN[ RUN      ]$NC $0\n"
+
+export LD_LIBRARY_PATH=${PWD}/../loader:${LD_LIBRARY_PATH}
+export VK_LAYER_PATH=${PWD}/../layersvt
+export VK_INSTANCE_LAYERS="VK_LAYER_LUNARG_device_simulation"
+
+# Test input datafile, and filename of output.
+FILENAME_01_IN="devsim_test1.json"
+FILENAME_01_OUT="device_simulation_layer_test_1.json"
+
+export VK_DEVSIM_FILENAME="${PWD}/${FILENAME_01_IN}"
+
+#export VK_DEVSIM_DEBUG_ENABLE="1"
+#export VK_DEVSIM_EXIT_ON_ERROR="1"
+#export VK_LOADER_DEBUG="all"
+
+VKJSON_INFO="${PWD}/../libs/vkjson/vkjson_info"
+
+${VKJSON_INFO} > /dev/null
+
+# compare vkjson output against all but last two lines of input
+NUM_LINES=$(cut -f1 -d' ' <(wc -l ${FILENAME_01_IN}))
+NUM_LINES=$(($NUM_LINES - 2))
+diff <(head -n ${NUM_LINES} ${FILENAME_01_IN}) <(head -n ${NUM_LINES} ${FILENAME_01_OUT}) >/dev/null
+RES=$?
+rm ${FILENAME_01_OUT}
+
+if [ "$RES" -eq 0 ] ; then
+   printf "$GREEN[  PASSED  ]$NC ${PGM}\n"
+else
+   printf "$RED[  FAILED  ]$NC file compare failed\n"
+   printf "TEST FAILED\n"
+   exit 1
+fi
+
+exit 0
+
+# vim: set sw=4 ts=8 et ic ai:
