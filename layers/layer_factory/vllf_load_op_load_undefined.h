6ba611e2f246004a5067cc06e45b9f8879e536c9
Mark Lobodzinski <mark@lunarg.com>
2017-12-04 15:14:26 -0700

    layers: Add load_op_load & Layout_Undefined intrceptr
    See Github #2248.
    
    Change-Id: I408361b3309878982c816d5aabd897fca12f867b

---
+++ layers/layer_factory/vllf_load_op_load_undefined.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "vector"
+#include "string"
+#include <algorithm>
+
+class LoadAndUndefined : public layer_factory {
+public:
+    // Constructor for state_tracker
+    LoadAndUndefined() : layer_factory(this) {};
+
+    // Intercept CreateRenderPassCalls calls and check LoadOp and Layout
+    VkResult PreCallCreateRenderPass(VkDevice device, const VkRenderPassCreateInfo *pCreateInfo,
+                                 const VkAllocationCallbacks *pAllocator, VkRenderPass *pRenderPass) {
+        for (uint32_t i = 0; i < pCreateInfo->attachmentCount; ++i) {
+            VkFormat format = pCreateInfo->pAttachments[i].format;
+            if (pCreateInfo->pAttachments[i].initialLayout == VK_IMAGE_LAYOUT_UNDEFINED) {
+                if ((FormatIsColor(format) || FormatHasDepth(format)) &&
+                    pCreateInfo->pAttachments[i].loadOp == VK_ATTACHMENT_LOAD_OP_LOAD) {
+                    Warning(
+                        "Render pass has an attachment with loadOp == VK_ATTACHMENT_LOAD_OP_LOAD and "
+                        "initialLayout == VK_IMAGE_LAYOUT_UNDEFINED.  This is probably not what you "
+                        "intended.  Consider using VK_ATTACHMENT_LOAD_OP_DONT_CARE instead if the "
+                        "image truely is undefined at the start of the render pass.");
+                }
+                if (FormatHasStencil(format) && pCreateInfo->pAttachments[i].stencilLoadOp == VK_ATTACHMENT_LOAD_OP_LOAD) {
+                    Warning(
+                        "Render pass has an attachment with stencilLoadOp == VK_ATTACHMENT_LOAD_OP_LOAD "
+                        "and initialLayout == VK_IMAGE_LAYOUT_UNDEFINED.  This is probably not what you "
+                        "intended.  Consider using VK_ATTACHMENT_LOAD_OP_DONT_CARE instead if the "
+                        "image truely is undefined at the start of the render pass.");
+                }
+            }
+        }
+        return VK_SUCCESS;
+    };
+};
+
+LoadAndUndefined load_op_load_and_layout_undefined;
