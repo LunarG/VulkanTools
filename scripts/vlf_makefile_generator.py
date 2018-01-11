qicny/Vulkan-LoaderAndValidationLayers

build: Add VLF build support for Android
Change-Id: Idc871bc8518311a9c015e763bdb9e49fa6adf166

scripts/vlf_makefile_generator.py
@@ -1,0 +1,104 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2015-2017 The Khronos Group Inc.
+# Copyright (c) 2015-2017 Valve Corporation
+# Copyright (c) 2015-2017 LunarG, Inc.
+# Copyright (c) 2015-2017 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Mark Lobodzinski <mark@lunarg.com>
+
+import os
+import subprocess
+import sys
+from glob import glob
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2):
+        print("Usage: %s <VLLF_DIR>" % sys.argv[0])
+        sys.exit(os.EX_USAGE)
+
+    # Write commit ID to output header file
+    with open("Android.mk", "w") as header_file:
+
+        # File Comment
+        file_comment = '# *** THIS FILE IS GENERATED - DO NOT EDIT ***\n'
+        file_comment += '# See vlf_makefile_generator.py for modifications\n'
+
+        header_file.write(file_comment)
+        # Copyright Notice
+        copyright = '\n'
+        copyright += '#  Copyright (c) 2015-2017 The Khronos Group Inc.\n'
+        copyright += '#  Copyright (c) 2015-2017 Valve Corporation\n'
+        copyright += '#  Copyright (c) 2015-2017 LunarG, Inc.\n'
+        copyright += '#  Copyright (c) 2015-2017 Google Inc.\n'
+        copyright += '#\n'
+        copyright += '#  Licensed under the Apache License, Version 2.0 (the "License");\n'
+        copyright += '#  you may not use this file except in compliance with the License.\n'
+        copyright += '#  You may obtain a copy of the License at\n'
+        copyright += '#\n'
+        copyright += '#      http://www.apache.org/licenses/LICENSE-2.0\n'
+        copyright += '#\n'
+        copyright += '#  Unless required by applicable law or agreed to in writing, software\n'
+        copyright += '#  distributed under the License is distributed on an "AS IS" BASIS,\n'
+        copyright += '#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n'
+        copyright += '#  See the License for the specific language governing permissions and\n'
+        copyright += '#  limitations under the License.\n'
+        copyright += '#\n'
+        copyright += '#  Author: Mark Lobodzinski <mark@lunarg.com>\n'
+        copyright += '#\n\n'
+        header_file.write(copyright)
+
+        # Destination directory is first (and only) arg
+        vllf_dir = sys.argv[1]
+
+        # Get list of subdirectories in layer_factory (dest) dir
+        layer_factory_path = "%s/*/" % vllf_dir
+        layer_factory_dirs = glob(layer_factory_path)
+
+        # Makefile source common to all factory layers
+        contents =  '#pragma once\n\n'
+        contents += 'LOCAL_PATH := $(call my-dir)\n'
+        contents += 'SRC_DIR := ../../..\n'
+        contents += 'LAYER_DIR := .\n'
+
+        # Output makefile target section for each factory layer in dest sub-directory
+        for layer_factory_path in layer_factory_dirs:
+            factory_layer = os.path.basename(os.path.normpath(layer_factory_path))
+            contents += '\n'
+            contents += 'include $(CLEAR_VARS)\n'
+            contents += 'LOCAL_MODULE := VkLayer_%s\n' % factory_layer
+            contents += 'LOCAL_SRC_FILES += $(LAYER_DIR)/layer_factory.cpp\n'
+            contents += 'LOCAL_SRC_FILES += $(SRC_DIR)/layers/vk_layer_table.cpp\n'
+            # Add *.cpp files (if any) to makefile dependencies
+            for path, subdirs, files in os.walk(factory_layer):
+                for file in files:
+                    if '.cpp' in file:
+                        contents += 'LOCAL_SRC_FILES += $(LOCAL_PATH)/$(SRC_DIR)/layers/layer_factory/%s/%s\n' & (factory_layer, file)
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/include\n'
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(LAYER_DIR)/include\n'
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/layers\n'
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/layers/layer_factory/%s\n' % factory_layer
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/loader\n'
+            contents += 'LOCAL_STATIC_LIBRARIES += layer_utils\n'
+            contents += 'LOCAL_CPPFLAGS += -std=c++11 -DVK_PROTOTYPES -Wall -Werror -Wno-unused-function -Wno-unused-const-variable\n'
+            contents += 'LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR -fvisibility=hidden\n'
+            contents += 'LOCAL_LDLIBS    := -llog\n'
+            contents += 'LOCAL_LDFLAGS   += -Wl,-Bsymbolic\n'
+            contents += 'LOCAL_LDFLAGS   += -Wl,--exclude-libs,ALL\n'
+            contents += 'include $(BUILD_SHARED_LIBRARY)\n'
+
+
+        header_file.write(contents)
+

LunarG/VulkanTools

scripts/vlf_makefile_generator.py
@@ -1,0 +1,102 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2015-2017 Valve Corporation
+# Copyright (c) 2015-2017 LunarG, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Author: Mark Lobodzinski <mark@lunarg.com>
+
+import os
+import subprocess
+import sys
+from glob import glob
+
+if __name__ == '__main__':
+    if (len(sys.argv) != 2):
+        print("Usage: %s <VLLF_DIR>" % sys.argv[0])
+        sys.exit(os.EX_USAGE)
+
+    # Write commit ID to output header file
+    with open("Android.mk", "w") as header_file:
+
+        # File Comment
+        file_comment = '# *** THIS FILE IS GENERATED - DO NOT EDIT ***\n'
+        file_comment += '# See vlf_makefile_generator.py for modifications\n'
+
+        header_file.write(file_comment)
+        # Copyright Notice
+        copyright = '\n'
+        copyright += '#  Copyright (c) 2015-2017 The Khronos Group Inc.\n'
+        copyright += '#  Copyright (c) 2015-2017 Valve Corporation\n'
+        copyright += '#  Copyright (c) 2015-2017 LunarG, Inc.\n'
+        copyright += '#  Copyright (c) 2015-2017 Google Inc.\n'
+        copyright += '#\n'
+        copyright += '#  Licensed under the Apache License, Version 2.0 (the "License");\n'
+        copyright += '#  you may not use this file except in compliance with the License.\n'
+        copyright += '#  You may obtain a copy of the License at\n'
+        copyright += '#\n'
+        copyright += '#      http://www.apache.org/licenses/LICENSE-2.0\n'
+        copyright += '#\n'
+        copyright += '#  Unless required by applicable law or agreed to in writing, software\n'
+        copyright += '#  distributed under the License is distributed on an "AS IS" BASIS,\n'
+        copyright += '#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n'
+        copyright += '#  See the License for the specific language governing permissions and\n'
+        copyright += '#  limitations under the License.\n'
+        copyright += '#\n'
+        copyright += '#  Author: Mark Lobodzinski <mark@lunarg.com>\n'
+        copyright += '#\n\n'
+        header_file.write(copyright)
+
+        # Destination directory is first (and only) arg
+        vlf_dir = sys.argv[1]
+
+        # Get list of subdirectories in layer_factory (dest) dir
+        layer_factory_path = "%s/*/" % vlf_dir
+        layer_factory_dirs = glob(layer_factory_path)
+
+        # Makefile source common to all factory layers
+        contents =  '#pragma once\n\n'
+        contents += 'LOCAL_PATH := $(call my-dir)\n'
+        contents += 'SRC_DIR := ../../..\n'
+        contents += 'LAYER_DIR := .\n'
+
+        # Output makefile target section for each factory layer in dest sub-directory
+        for layer_factory_path in layer_factory_dirs:
+            factory_layer = os.path.basename(os.path.normpath(layer_factory_path))
+            contents += '\n'
+            contents += 'include $(CLEAR_VARS)\n'
+            contents += 'LOCAL_MODULE := VkLayer_%s\n' % factory_layer
+            contents += 'LOCAL_SRC_FILES += $(LAYER_DIR)/layer_factory.cpp\n'
+            contents += 'LOCAL_SRC_FILES += $(SRC_DIR)/layers/vk_layer_table.cpp\n'
+            # Add *.cpp files (if any) to makefile dependencies
+            for path, subdirs, files in os.walk(factory_layer):
+                for file in files:
+                    if '.cpp' in file:
+                        contents += 'LOCAL_SRC_FILES += $(LOCAL_PATH)/$(SRC_DIR)/layers/layer_factory/%s/%s\n' & (factory_layer, file)
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/include\n'
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(LAYER_DIR)/include\n'
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/layers\n'
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/layers/layer_factory/%s\n' % factory_layer
+            contents += 'LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(SRC_DIR)/loader\n'
+            contents += 'LOCAL_STATIC_LIBRARIES += layer_utils\n'
+            contents += 'LOCAL_CPPFLAGS += -std=c++11 -DVK_PROTOTYPES -Wall -Werror -Wno-unused-function -Wno-unused-const-variable\n'
+            contents += 'LOCAL_CPPFLAGS += -DVK_USE_PLATFORM_ANDROID_KHR -fvisibility=hidden\n'
+            contents += 'LOCAL_LDLIBS    := -llog\n'
+            contents += 'LOCAL_LDFLAGS   += -Wl,-Bsymbolic\n'
+            contents += 'LOCAL_LDFLAGS   += -Wl,--exclude-libs,ALL\n'
+            contents += 'include $(BUILD_SHARED_LIBRARY)\n'
+
+
+        header_file.write(contents)
+