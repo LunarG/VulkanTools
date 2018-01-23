b6598b9307602f03c55653c365d72ca17583c00b
Karl Schultz <karl@lunarg.com>
2018-01-23 13:59:03 -0700

    build: Adapt build files for LVL submodule
    - New update_external_sources.sh
    - Fix execute permissions on a script
    
    Change-Id: I86d60a4cbb1c33c4b3d5ae6608802c4a53d47347

--- update_external_sources.bat
+++ update_external_sources.bat
@@ -2,26 +2,18 @@
 
 set errorCode=0
 set BUILD_DIR=%~dp0
-set BASE_DIR="%BUILD_DIR%external"
-set REVISION_DIR="%BUILD_DIR%external_revisions"
+
+set BASE_DIR=%BUILD_DIR%submodules
 set JSONCPP_DIR=%BASE_DIR%\jsoncpp
-
-set /p JSONCPP_REVISION= < %REVISION_DIR%\jsoncpp_revision
-
-echo JSONCPP_REVISION=%JSONCPP_REVISION%
+set V_LVL_DIR=%BASE_DIR%\Vulkan-LoaderAndValidationLayers
 
 
-:update_jsoncpp
-   echo.
-   echo Updating %JSONCPP_DIR%
-   cd %JSONCPP_DIR%
-   git fetch --all
-   git checkout %JSONCPP_REVISION%
+git submodule update --init --recursive
 
 :build_jsoncpp
    echo.
    echo Building %JSONCPP_DIR%
-   cd  %JSONCPP_DIR%
+   cd  "%JSONCPP_DIR%"
    python amalgamate.py
 
    if not exist %JSONCPP_DIR%\dist\json\json.h (
@@ -29,4 +21,10 @@ echo JSONCPP_REVISION=%JSONCPP_REVISION%
       echo JsonCPP Amalgamation failed to generate %JSONCPP_DIR%\dist\json\json.h
       set errorCode=1
    )
+
+:build_lvl
+    echo.
+    echo Setting Up %V_LVL_DIR%
+    cd "%V_LVL_DIR%"
+    call .\update_external_sources.bat
 
