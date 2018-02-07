d6bfaa7147fe179946d2297241c4f5b709d8d769
Tony-LunarG <tony@lunarg.com>
2018-02-07 14:12:33 -0700

    Fix trace_positive_validation for submodules
    Also fix error checking for use once errors are fixed / skipped

--- tests/trace_positive_validation.ps1
+++ tests/trace_positive_validation.ps1
@@ -15,22 +15,32 @@ trap {
 }
 
 $skip = @( "LongFenceChain)" )
+$lvlbinpath = "$toolsroot\build\submodules\Vulkan-LoaderAndValidationLayers"
+$lvlcodepath = "$toolsroot\submodules\Vulkan-LoaderAndValidationLayers"
 
 if ( -not($toolsroot) ) { Throw "You must supply a path to a VulkanTools build via -toolsroot" }
 
-$env:VK_LAYER_PATH = "$toolsroot\build\layers\$build;$toolsroot\build\layersvt\$build"
+$env:VK_LAYER_PATH = "$lvlbinpath\layers\$build;$toolsroot\build\layersvt\$build"
 
-$lines = Select-String -Path $toolsroot\tests\layer_validation_tests.cpp -Pattern "VkPositiveLayerTest,"
+$lines = Select-String -Path $lvlcodepath\tests\layer_validation_tests.cpp -Pattern "VkPositiveLayerTest,"
 foreach ($line in $lines) {
     $left = $line -replace ".*, "
     $test = $left -replace "\) {"
     if ( -Not $skip.Contains($test)) {
-        $command = "$toolsroot\build\vktrace\$build\vktrace -p $toolsroot\build\tests\$build\vk_layer_validation_tests.exe -o positive.vktrace -a ""--gtest_filter=VkPositiveLayerTest.$test"""
+        $command = "$toolsroot\build\vktrace\$build\vktrace -p $lvlbinpath\tests\$build\vk_layer_validation_tests.exe -o positive.vktrace -a ""--gtest_filter=VkPositiveLayerTest.$test"""
         echo $command
         Invoke-Expression $command
+        #if ($lastExitCode) {
+        #    Write-Error $lastExitCode
+        #    exit 1
+        #}
         $command = "$toolsroot\build\vktrace\$build\vkreplay -t positive.vktrace"
         echo $command
         Invoke-Expression $command
+        #if ($lastExitCode) {
+        #    Write-Error $lastExitCode
+        #    exit 1
+        #}
         rm positive.vktrace
     }
 }
