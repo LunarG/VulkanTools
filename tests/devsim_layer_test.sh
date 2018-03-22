c6468277fab6c11b2b2341f64eab40af239583b6
Mike Weiblen <mikew@lunarg.com>
2018-03-22 13:59:51 -0600

    devsim: Restructure test script, add 2nd test
    Several refinements to the devsim test script:
    - use jq to extract and sort elements of output files.
    - better error reporting.
    - fix several bugs.
    
    Add test case 2 using new "vulkaninfo -j" feature:
    - use jq to compare JSON files.
    - new devsim JSON input/gold files.
    - update CMakeLists for new files.
    
    Leave test case 1 (using vkjson_info) in place for now.
    
    Temporarily disable ArrayOfVkQueueFamilyProperties checking,
    while awaiting fix for vulkaninfo issue.
    
    Change-Id: I2233f5e338fd1e8273c2f3c2046e3ca013d94f0b

--- tests/devsim_layer_test.sh
+++ tests/devsim_layer_test.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
-# Simple test of devsim layer
-# Uses 'jq' v1.5 (https://stedolan.github.io/jq/) to extract sections of
-# test's JSON output and reformat for consistent comparison with gold file.
+# Tests of LunarG Device Simulation (devsim) layer
+# Uses 'jq' v1.5 https://stedolan.github.io/jq/
 
-set errexit
-set nounset
-set physical
+set -o nounset
+set -o physical
 
 cd $(dirname "${BASH_SOURCE[0]}")
+LVL_BUILD_DIR="$PWD/../submodules/Vulkan-LoaderAndValidationLayers"
 
 if [ -t 1 ] ; then
     RED='\033[0;31m'
@@ -18,48 +17,62 @@ else
     GREEN=''
     NC=''
 fi
-
 printf "$GREEN[ RUN      ]$NC $0\n"
 
-export LD_LIBRARY_PATH=${PWD}/../submodules/Vulkan-LoaderAndValidationLayers/loader:${LD_LIBRARY_PATH}
-export VK_LAYER_PATH=${PWD}/../layersvt
+function fail_msg () {
+   printf "$RED[  FAILED  ]$NC $1 failed\n"
+   exit 1
+}
+
+export LD_LIBRARY_PATH="$LVL_BUILD_DIR/loader:${LD_LIBRARY_PATH:-}"
+export VK_LAYER_PATH="$PWD/../layersvt"
 export VK_INSTANCE_LAYERS="VK_LAYER_LUNARG_device_simulation"
 
 #export VK_DEVSIM_DEBUG_ENABLE="1"
 #export VK_DEVSIM_EXIT_ON_ERROR="1"
 #export VK_LOADER_DEBUG="all"
 
-VKJSON_INFO="${PWD}/../submodules/Vulkan-LoaderAndValidationLayers/libs/vkjson/vkjson_info"
-
 #############################################################################
-# Test #1 input datafile, and filename of output.
+# Test 1: Verify devsim results using vkjson_info.
 
-FILENAME_01_IN="devsim_test1_in_ArrayOfVkFormatProperties.json:devsim_test1_in.json"
-FILENAME_01_GOLD="devsim_test1_gold.json"
 FILENAME_01_RESULT="device_simulation_layer_test_1.json"
 FILENAME_01_STDOUT="device_simulation_layer_test_1.txt"
+FILENAME_01_TEMP1="devsim_test1_temp1.json"
+rm -f $FILENAME_01_RESULT $FILENAME_01_STDOUT $FILENAME_01_TEMP1
 
-export VK_DEVSIM_FILENAME="${FILENAME_01_IN}"
-${VKJSON_INFO} > ${FILENAME_01_STDOUT}
+export VK_DEVSIM_FILENAME="devsim_test1_in_ArrayOfVkFormatProperties.json:devsim_test1_in.json"
+"$LVL_BUILD_DIR/libs/vkjson/vkjson_info" > $FILENAME_01_STDOUT
+[ $? -eq 0 ] || fail_msg "test1 vkjson_info"
 
-# reformat/extract/sort vkjson output using jq, then compare against gold.
-diff ${FILENAME_01_GOLD} \
-    <(jq -S '{properties,features,memory,queues,formats}' ${FILENAME_01_RESULT}) \
-    >> ${FILENAME_01_STDOUT}
-RES=$?
-rm ${FILENAME_01_RESULT}
-rm ${FILENAME_01_STDOUT}
+# Use jq to extract, reformat, and sort the output.
+jq -S '{properties,features,memory,queues,formats}' $FILENAME_01_RESULT > $FILENAME_01_TEMP1
+[ $? -eq 0 ] || fail_msg "test1 jq extraction"
+
+diff devsim_test1_gold.json $FILENAME_01_TEMP1 >> $FILENAME_01_STDOUT
+[ $? -eq 0 ] || fail_msg "test1 diff comparison"
+
+#############################################################################
+# Test 2: Verify devsim results using vulkaninfo.
+
+FILENAME_02_TEMP1="devsim_test2_temp1.json"
+FILENAME_02_TEMP2="devsim_test2_temp2.json"
+rm -f $FILENAME_02_TEMP1 $FILENAME_02_TEMP2
+
+export VK_DEVSIM_FILENAME="devsim_test2_in1.json:devsim_test2_in2.json:devsim_test2_in3.json:devsim_test2_in4.json:devsim_test2_in5.json"
+"$LVL_BUILD_DIR/demos/vulkaninfo" -j > $FILENAME_02_TEMP1 2> /dev/null
+[ $? -eq 0 ] || fail_msg "test2 vulkaninfo"
+
+# Use jq to extract, reformat, and sort the output.
+JSON_SECTIONS='{VkPhysicalDeviceProperties,VkPhysicalDeviceFeatures,VkPhysicalDeviceMemoryProperties,ArrayOfVkFormatProperties}'
+jq -S $JSON_SECTIONS $FILENAME_02_TEMP1 > $FILENAME_02_TEMP2
+[ $? -eq 0 ] || fail_msg "test2 jq extraction"
+
+jq --slurp  --exit-status '.[0] == .[1]' devsim_test2_gold.json $FILENAME_02_TEMP2 > /dev/null
+[ $? -eq 0 ] || fail_msg "test2 jq comparison"
 
 #############################################################################
 
-if [ "$RES" -eq 0 ] ; then
-   printf "$GREEN[  PASSED  ]$NC ${PGM}\n"
-else
-   printf "$RED[  FAILED  ]$NC file compare failed\n"
-   printf "TEST FAILED\n"
-   exit 1
-fi
-
+printf "$GREEN[  PASSED  ]$NC $0\n"
 exit 0
 
 # vim: set sw=4 ts=8 et ic ai:
