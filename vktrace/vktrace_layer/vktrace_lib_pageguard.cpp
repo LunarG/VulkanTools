d6155a4a7a49bd57d03f448f9714f3a98488668e
Justus Wang <justus.wang@arm.com>
2018-05-28 17:36:20 +0800

    vktrace: Fix lock on locked mutex lock
    vktrace_delete_trace_packet() which will leave critical section
    s_trace_lock is not always being called after CREATE_TRACE_PACKET
    which will enter critical section s_trace_lock in:
    
    1. vkFlushMappedMemoryRangesWithoutAPICall()
    2. generateMapUnmap()
    3. generate a vkMapMemory to recreate the persistently mapped buffers
    in snapshot_state_tracker()
    
    This will cause missing "leave critical section" or lock on an already
    locked mutex lock.
    
    This change fixed it by always call vktrace_delete_trace_packet() after
    CREATE_TRACE_PACKET.

--- vktrace/vktrace_layer/vktrace_lib_pageguard.cpp
+++ vktrace/vktrace_layer/vktrace_lib_pageguard.cpp
@@ -494,15 +494,14 @@ VkResult vkFlushMappedMemoryRangesWithoutAPICall(VkDevice device, uint32_t memor
     if (!g_trimEnabled) {
         // trim not enabled, send packet as usual
         FINISH_TRACE_PACKET();
-    } else if (g_trimIsPreTrim) {
+    } else {
         vktrace_finalize_trace_packet(pHeader);
-    } else if (g_trimIsInTrim) {
-        // Currently tracing the frame, so need to track references & store packet to write post-tracing.
-        vktrace_finalize_trace_packet(pHeader);
-        trim::write_packet(pHeader);
-    } else  // g_trimIsPostTrim
-    {
-        vktrace_delete_trace_packet(&pHeader);
+        if (g_trimIsInTrim) {
+            // Currently tracing the frame, so need to track references & store packet to write post-tracing.
+            trim::write_packet(pHeader);
+        } else {
+            vktrace_delete_trace_packet(&pHeader);
+        }
     }
     return result;
 }
