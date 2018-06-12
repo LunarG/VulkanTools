0903b4e66d6df813933db318cc06ef2ab66b82e0
Joey Bzdek <joey@lunarg.com>
2018-06-12 11:37:03 -0600

    tests: Remove redundancies from vktrace scripts
    Remove duplicated functionality from the OS-specific scripts for setting
    up the vktracereplay tests. Eventually these will be removed.
    
    Change-Id: I3d74b4034621885a71ee54dfd14258488d0f4b9e

--- tests/vktracereplay.sh
+++ tests/vktracereplay.sh
@@ -14,40 +14,15 @@ fi
 
 printf "$GREEN[ RUN      ]$NC $0\n"
 
-export VK_LAYER_PATH=${PWD}/../layersvt
+python3 vktracereplay.py "" ${PWD}/../vktrace/vktrace ${PWD}/../layersvt ${PWD}/../vktrace/vkreplay
 
-function trace_replay {
-	PGM=`which $1`
-	PARGS=$2
-	TARGS=$3
-	VKTRACE=${PWD}/../vktrace/vktrace
-	VKREPLAY=${PWD}/../vktrace/vkreplay
-	printf "$GREEN[ TRACE    ]$NC ${PGM}\n"
-	${VKTRACE}	--Program ${PGM} \
-			--Arguments "--c 100 ${PARGS}" \
-			--WorkingDir ${PWD} \
-			--OutputTrace ${PGM}.vktrace \
-			${TARGS} \
-			-s 1
-	mv 1.ppm 1-trace.ppm
-	printf "$GREEN[ REPLAY   ]$NC ${PGM}\n"
-	${VKREPLAY}	--Open ${PGM}.vktrace \
-			-s 1
-	rm -f ${PGM}.vktrace
-	cmp -s 1.ppm 1-trace.ppm
-	RES=$?
-	rm 1.ppm 1-trace.ppm
-	if [ $RES -eq 0 ] ; then
-	   printf "$GREEN[  PASSED  ]$NC ${PGM}\n"
-	else
-	   printf "$RED[  FAILED  ]$NC screenshot file compare failed\n"
-	   printf "$RED[  FAILED  ]$NC ${PGM}\n"
-	   printf "TEST FAILED\n"
-	   exit 1
-	fi
-}
-
-trace_replay cube "" "--PMB false"
+if [ $? -eq 0 ] ; then
+	printf "$GREEN[  PASSED  ]$NC ${PGM}\n"
+else
+	printf "$RED[  FAILED  ]$NC Trace/replay tests failed.\n"
+	printf "$RED[  FAILED  ]$NC ${PGM}\n"
+	printf "TEST FAILED\n"
+	exit 1
+fi
 
 exit 0
-
