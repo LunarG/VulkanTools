698b9452e248312e8ff20c52a7bb1d4ea8e5a466
Dror Smolarsky <dror.smolarsky@gmail.com>
2018-10-29 14:28:21 -0400

    build. Add a python build script to simplify builds.
    The build script updats all dependencies, and runs both cmake build file
    generation and build command.
    No additioanl prepration step, or external repositories are needd to
    build VulkanTools when using the script
    The script currently handles windows + linux, debug + release, x86 + x64 builds.
    Each arch + config build will happen in a separate directory.
    
    Change-Id: If545bad4af97aa5b9e1e15ddda6d59e5e165857e

---
+++ build.py
@@ -0,0 +1,54 @@
+# =============================================================
+# Copyright (c) 2018 Advanced Micro Devices, Inc.
+# =============================================================
+
+import argparse
+import os
+import os.path
+import subprocess
+import sys
+
+if __name__ == '__main__':
+    arg_parser = argparse.ArgumentParser(
+        description='Vulkan Tools build script')
+    arg_parser.add_argument(
+        '-a', '--arch', action='store',
+        default='x64',  choices=['x86', 'x64'],
+        dest='arch', metavar='ARCH',
+        help='Choose target architecture')
+    arg_parser.add_argument(
+        '-c', '--config', action='store',
+        default='release',  choices=['debug', 'release'],
+        dest='config', metavar='CONFIG',
+        help='Choose target build configuration')
+    arg_parser.add_argument(
+        '-i', '--install', action='store_true',
+        default=False,
+        dest='install',
+        help='Build install target')
+    arguments = arg_parser.parse_args(sys.argv[1:])
+    subprocess.run(['git', 'submodule', 'update', '--recursive'])
+    subprocess.run(['update_external_sources.bat'])
+    build_dir = 'build'
+    if 'debug' == arguments.config:
+        build_dir = 'dbuild'
+    if 'x86' == arguments.arch:
+        build_dir = build_dir + '32'
+    build_path = os.path.join(os.getcwd(), build_dir)
+    print(build_path, arguments.config.capitalize())
+    os.makedirs(build_path, mode=0o744, exist_ok=True)
+    subprocess.run([
+        'python', os.path.join('..', 'scripts', 'update_deps.py'),
+        '--config', arguments.config.capitalize(),
+        '--arch', arguments.arch],
+        cwd=build_path)
+    subprocess.run(
+        ['cmake ',
+         '-A', arguments.arch, '--config', arguments.config.capitalize(),
+         '-C', 'helper.cmake', os.getcwd()],
+        cwd=build_path)
+    cmake_build_args = ['cmake', '--build', '.',
+                        '--config', arguments.config.capitalize()]
+    if arguments.install:
+        cmake_build_args.extend(['--target', 'install'])
+    subprocess.run(cmake_build_args, cwd=build_path)
