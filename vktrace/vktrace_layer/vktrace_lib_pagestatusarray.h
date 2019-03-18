5e60999eed987e5b6bff2ff7daa7de534d87e85d
mizhen <ming.zheng@amd.com>
2019-03-18 16:26:03 -0400

    vktrace: use memory write watch and host memory extension to capture
    The change enable user to use memory write watch and external host
    memory extension to capture a title. It remove the shadow memory from
    the capture process and let target title use host memory for all host
    visible memory object.
    
    VKTRACE-111
    
    Change-Id: I1920a2d9ed0f9271b21d0027897844f1b5b0684f

--- vktrace/vktrace_layer/vktrace_lib_pagestatusarray.h
+++ vktrace/vktrace_layer/vktrace_lib_pagestatusarray.h
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2016 Advanced Micro Devices, Inc. All rights reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (c) 2016-2019 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #pragma once
 
 #include <stdbool.h>
@@ -53,6 +53,7 @@ typedef class PageStatusArray {
     void backupChangedArray();
     void backupReadArray();
     void clearAll();
+    void clearActiveChangesArray();
 
    private:
     const static uint64_t PAGE_FLAG_AMOUNT_PER_BYTE;
