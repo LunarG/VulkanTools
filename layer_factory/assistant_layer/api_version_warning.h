be36bb8578ad8a066a8b90fb84d192704f639f4e
Camden <camden@lunarg.com>
2019-07-01 11:33:53 -0600

    asst-layer: Add API version number to warning
    Change-Id: I17192300b0de3743cebe283e7de2c7bd1353b16f

--- layer_factory/assistant_layer/api_version_warning.h
+++ layer_factory/assistant_layer/api_version_warning.h
@@ -21,16 +21,31 @@
 
 #include <sstream>
 #include <iostream>
+#include <iomanip>
 
 class ApiVersionWarning : public layer_factory {
    public:
     // Constructor for interceptor
     ApiVersionWarning(){};
 
+    // get the API name is proper format
+    std::string GetAPIVersionName(uint32_t version) {
+        std::stringstream version_name;
+        uint32_t major = VK_VERSION_MAJOR(version);
+        uint32_t minor = VK_VERSION_MINOR(version);
+        uint32_t patch = VK_VERSION_PATCH(version);
+
+        version_name << major << "." << minor << "." << patch << " (0x" << std::setfill('0') << std::setw(8) << std::hex << version
+                     << ")";
+
+        return version_name.str();
+    }
+
     // Intercept CreateInstance to capture API Version
     VkResult PreCallCreateInstance(const VkInstanceCreateInfo* pCreateInfo, const VkAllocationCallbacks* pAllocator,
                                    VkInstance* pInstance) {
         instance_api_version = pCreateInfo->pApplicationInfo->apiVersion;
+
         return VK_SUCCESS;
     }
 
@@ -40,12 +55,18 @@ class ApiVersionWarning : public layer_factory {
         instance_layer_data* instance_data = GetLayerDataPtr(get_dispatch_key(physicalDevice), instance_layer_data_map);
         VkPhysicalDeviceProperties physical_device_properties{};
         instance_data->dispatch_table.GetPhysicalDeviceProperties(physicalDevice, &physical_device_properties);
+        device_api_version = physical_device_properties.apiVersion;
 
         // check api versions and warn if instance api Version is higher than version on device.
         // NOTE: Should be able to use ">" operator as other places in validation layers does so
-        if (instance_api_version > physical_device_properties.apiVersion) {
+        if (instance_api_version > device_api_version) {
             std::stringstream message;
-            message << "vkCreateDevice: API Version of current instance is higher than API Version on device.";
+            std::string inst_api_name = GetAPIVersionName(instance_api_version);
+            std::string dev_api_name = GetAPIVersionName(device_api_version);
+
+            message << "vkCreateDevice: API Version of current instance, " << inst_api_name
+                    << " is higher than API Version on device, " << dev_api_name << ".";
+            std::cout << message.str() << std::endl;
             Warning(message.str());
         }
 
@@ -53,8 +74,9 @@ class ApiVersionWarning : public layer_factory {
     }
 
    private:
-    // api version for the instance that was created
+    // api version for the instance and device
     uint32_t instance_api_version;
+    uint32_t device_api_version;
 };
 
 ApiVersionWarning api_version_warning;
