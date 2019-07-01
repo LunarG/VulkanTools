ed887f2faf244e6747bea1af7defdf0bc646433c
Camden <camden@lunarg.com>
2019-07-01 13:13:57 -0600

    asst_layer: Add more Get checks
    Change-Id: I4cdb9b2cdc5be41b2d6e0a12bb4e0f759d45b4b8

--- layer_factory/assistant_layer/skip_get_call_warning.cpp
+++ layer_factory/assistant_layer/skip_get_call_warning.cpp
@@ -2,9 +2,6 @@
 #include <sstream>
 #include <iostream>
 
-// TODO: REMOVE METHOD. THIS IS JUST FOR TESTING
-void SkipGetCallWarning::PreCallApiFunction(const char *api_name) { printf("Calling %s\n", api_name); }
-
 PHYSICAL_DEVICE_STATE *SkipGetCallWarning::GetPhysicalDeviceState() { return physical_device_state; }
 
 PHYSICAL_DEVICE_STATE *SkipGetCallWarning::GetPhysicalDeviceState(VkPhysicalDevice pd) {
@@ -16,12 +13,21 @@ PHYSICAL_DEVICE_STATE *SkipGetCallWarning::GetPhysicalDeviceState(VkPhysicalDevi
     return &physicalDevice->second;
 }
 
+// helper function that sets the state of any get call that has a count check as well
+template <typename T, typename S>
+void SetStateWithCount(T& call_state, S pStruct) {
+    if (!pStruct) {
+        if (call_state == UNCALLED) call_state = QUERY_COUNT;
+    } else {
+        call_state = QUERY_DETAILS;
+    }
+}
+
 // Populate physical_device_map with al the Physical Devices
 VkResult SkipGetCallWarning::PostCallEnumeratePhysicalDevices(VkInstance instance, uint32_t* pPhysicalDeviceCount,
                                                               VkPhysicalDevice* pPhysicalDevices, VkResult result) {
     if ((pPhysicalDevices != NULL) && ((result == VK_SUCCESS || result == VK_INCOMPLETE))) {
         for (uint32_t i = 0; i < *pPhysicalDeviceCount; i++) {
-            // TODO: Look at moving this to the [] accessor. Less readable but better complexity maybe.
             PHYSICAL_DEVICE_STATE pd_state;
             physical_device_map.insert(std::make_pair(pPhysicalDevices[i], pd_state));
         }
@@ -42,9 +48,9 @@ VkResult SkipGetCallWarning::PreCallCreateDevice(VkPhysicalDevice physicalDevice
     auto pd_state = GetPhysicalDeviceState(physicalDevice);
     physical_device_state = pd_state;
 
-    if (pd_state->vkGetPhysicalDeviceFeaturesState == UNCALLED) {
+    if ((pd_state->vkGetPhysicalDeviceFeaturesState == UNCALLED) && (pCreateInfo->pEnabledFeatures != NULL)) {
         std::stringstream message;
-        message << "vkCreateDevice caled before getting physical device features from vkGetPhysicalDeviceFeatures().";
+        message << "vkCreateDevice called before getting physical device features from vkGetPhysicalDeviceFeatures().";
         Warning(message.str());
     }
 
@@ -65,11 +71,7 @@ VkResult SkipGetCallWarning::PreCallGetPhysicalDeviceSurfacePresentModesKHR(VkPh
     auto physical_device_state = GetPhysicalDeviceState(physicalDevice);
     auto& call_state = physical_device_state->vkGetPhysicalDeviceSurfacePresentModesKHRState;
 
-    if (!pPresentModes) {
-        if (call_state == UNCALLED) call_state = QUERY_COUNT;
-    } else {
-        call_state = QUERY_DETAILS;
-    }
+    SetStateWithCount(call_state, pPresentModes);
 
     return VK_SUCCESS;
 }
@@ -80,15 +82,12 @@ VkResult SkipGetCallWarning::PreCallGetPhysicalDeviceSurfaceFormatsKHR(VkPhysica
     auto physical_device_state = GetPhysicalDeviceState(physicalDevice);
     auto& call_state = physical_device_state->vkGetPhysicalDeviceSurfaceFormatsKHRState;
 
-    if (!pSurfaceFormats) {
-        if (call_state == UNCALLED) call_state = QUERY_COUNT;
-    } else {
-        call_state = QUERY_DETAILS;
-    }
+    SetStateWithCount(call_state, pSurfaceFormats);
 
     return VK_SUCCESS;
 }
 
+// check that makes sure all necessary get calls are made before creating the swapchain
 VkResult SkipGetCallWarning::PreCallCreateSwapchainKHR(VkDevice device, const VkSwapchainCreateInfoKHR* pCreateInfo,
                                                        const VkAllocationCallbacks* pAllocator, VkSwapchainKHR* pSwapchain) {
     auto physical_device_state = GetPhysicalDeviceState();
@@ -110,6 +109,91 @@ VkResult SkipGetCallWarning::PreCallCreateSwapchainKHR(VkDevice device, const Vk
     if (physical_device_state->vkGetPhysicalDeviceSurfaceFormatsKHRState != QUERY_DETAILS) {
         std::stringstream message;
         message << "vkCreateSwapchainKHR called before getting surface format(s) from vkGetPhysicalDeviceSurfaceFormatsKHR().";
+        Warning(message.str());
+    }
+
+    return VK_SUCCESS;
+}
+
+VkResult SkipGetCallWarning::PreCallGetPhysicalDeviceDisplayPlanePropertiesKHR(VkPhysicalDevice physicalDevice,
+                                                                               uint32_t* pPropertyCount,
+                                                                               VkDisplayPlanePropertiesKHR* pProperties) {
+    auto physical_device_state = GetPhysicalDeviceState(physicalDevice);
+    auto& call_state = physical_device_state->vkGetPhysicalDeviceDisplayPlanePropertiesKHRState;
+
+    SetStateWithCount(call_state, pProperties);
+
+    return VK_SUCCESS;
+}
+
+VkResult SkipGetCallWarning::PreCallGetDisplayPlaneSupportedDisplaysKHR(VkPhysicalDevice physicalDevice, uint32_t planeIndex,
+                                                                        uint32_t* pDisplayCount, VkDisplayKHR* pDisplays) {
+    auto physical_device_state = GetPhysicalDeviceState();
+
+    if (physical_device_state->vkGetPhysicalDeviceDisplayPlanePropertiesKHRState != QUERY_DETAILS) {
+        std::stringstream message;
+        message << "vkGetDisplayPlaneSupportedDiesplaysKHR called before getting diplay plane properties from "
+                   "vkGetPhysicalDeviceDisplayPlanePropertiesKHR().";
+        Warning(message.str());
+    }
+
+    return VK_SUCCESS;
+}
+
+void SkipGetCallWarning::PreCallGetPhysicalDeviceMemoryProperties(VkPhysicalDevice physicalDevice,
+                                                                  VkPhysicalDeviceMemoryProperties* pMemoryProperties) {
+    auto physical_device_state = GetPhysicalDeviceState(physicalDevice);
+    physical_device_state->vkGetPhysicalDeviceMemoryPropertiesState = QUERY_DETAILS;
+}
+
+VkResult SkipGetCallWarning::PreCallAllocateMemory(VkDevice device, const VkMemoryAllocateInfo* pAllocateInfo,
+                                                   const VkAllocationCallbacks* pAllocator, VkDeviceMemory* pMemory) {
+    auto physical_device_state = GetPhysicalDeviceState();
+
+    if (physical_device_state->vkGetPhysicalDeviceMemoryPropertiesState == UNCALLED) {
+        std::stringstream message;
+        message << "vkAllocateMemory called before retrieving memoryHeaps[pAllocateInfo->memoryTypeIndex].size and memoryTypeCount "
+                   "from vkGetPhysicalDeviceMemoryProperties().";
+        Warning(message.str());
+    }
+
+    return VK_SUCCESS;
+}
+
+void SkipGetCallWarning::PreCallGetBufferMemoryRequirements(VkDevice device, VkBuffer buffer,
+                                                            VkMemoryRequirements* pMemoryRequirements) {
+    auto physical_device_state = GetPhysicalDeviceState();
+    physical_device_state->vkGetBufferMemoryRequirementsState = QUERY_DETAILS;
+}
+
+void SkipGetCallWarning::PreCallGetImageMemoryRequirements(VkDevice device, VkImage image,
+                                                           VkMemoryRequirements* pMemoryRequirements) {
+    auto physical_device_state = GetPhysicalDeviceState();
+    physical_device_state->vkGetImageMemoryRequirementsState = QUERY_DETAILS;
+}
+
+VkResult SkipGetCallWarning::PreCallBindBufferMemory(VkDevice device, VkBuffer buffer, VkDeviceMemory memory,
+                                                     VkDeviceSize memoryOffset) {
+    auto physical_device_state = GetPhysicalDeviceState();
+
+    if (physical_device_state->vkGetBufferMemoryRequirementsState == UNCALLED) {
+        std::stringstream message;
+        message << "vkBindBufferMemory called before retrieving memoryTypeBits, alignment, and size in the VkMemoryRequirements "
+                   "structure returned from vkGetBufferMemoryRequirements().";
+        Warning(message.str());
+    }
+
+    return VK_SUCCESS;
+}
+
+VkResult SkipGetCallWarning::PreCallBindImageMemory(VkDevice device, VkImage image, VkDeviceMemory memory,
+                                                    VkDeviceSize memoryOffset) {
+    auto physical_device_state = GetPhysicalDeviceState();
+
+    if (physical_device_state->vkGetImageMemoryRequirementsState == UNCALLED) {
+        std::stringstream message;
+        message << "vkBindImageMemory called before retrieving memoryTypeBits, alignment, and size in the VkMemoryRequirements "
+                   "structure returned fro vkGetImagememoryRequirements().";
         Warning(message.str());
     }
 
