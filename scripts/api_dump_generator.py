eb4041c21b305211a471a544119d2b0dae07ade8
Charles Giessen <charles@lunarg.com>
2019-07-18 11:51:16 -0600

    Made timestamps disabled by default, replaced lowercase greek m with u, fixed colons in text output
    Change-Id: I190c4304739b5dae53f1ddac2471063df33fe319

--- scripts/api_dump_generator.py
+++ scripts/api_dump_generator.py
@@ -723,10 +723,11 @@ std::ostream& dump_text_{funcName}(ApiDumpInstance& dump_inst, {funcReturn} resu
 {{
     const ApiDumpSettings& settings(dump_inst.settings());
     settings.stream() << "Thread " << dump_inst.threadID() << ", Frame " << dump_inst.frameCount();
-    if(settings.showTimestamp())
-        settings.stream() << ", Time " << dump_inst.current_time_since_start().count() << " μs\\n";
-    else 
-        settings.stream() << "\\n";
+    if(settings.showTimestamp()) {{
+        settings.stream() << ", Time " << dump_inst.current_time_since_start().count() << " us:\\n";
+    }} else {{
+        settings.stream() << ":\\n";
+    }} 
     settings.stream() << "{funcName}({funcNamedParams}) returns {funcReturn} ";
     dump_text_{funcReturn}(result, settings, 0) << ":\\n";
     if(settings.showParams())
@@ -754,10 +755,11 @@ std::ostream& dump_text_{funcName}(ApiDumpInstance& dump_inst, {funcTypedParams}
 {{
     const ApiDumpSettings& settings(dump_inst.settings());
     settings.stream() << "Thread " << dump_inst.threadID() << ", Frame " << dump_inst.frameCount();
-    if(settings.showTimestamp())
-        settings.stream() << ", Time " << dump_inst.current_time_since_start().count() << " μs\\n";
-    else 
-        settings.stream() << "\\n";
+    if(settings.showTimestamp()) {{
+        settings.stream() << ", Time " << dump_inst.current_time_since_start().count() << " us:\\n";
+    }} else {{
+        settings.stream() << ":\\n";
+    }} 
     settings.stream() << "{funcName}({funcNamedParams}) returns {funcReturn}:\\n";
 
     if(settings.showParams())
@@ -1137,7 +1139,7 @@ std::ostream& dump_html_{funcName}(ApiDumpInstance& dump_inst, {funcReturn} resu
 
     settings.stream() << "<div class='thd'>Thread " << dump_inst.threadID() << ":</div>";
     if(settings.showTimestamp())
-        settings.stream() << "<div class='time'>Time: " << dump_inst.current_time_since_start().count() << " μs</div>";
+        settings.stream() << "<div class='time'>Time: " << dump_inst.current_time_since_start().count() << " us</div>";
     settings.stream() << "<details class='fn'><summary>";
     dump_html_nametype(settings.stream(), settings.showType(), "{funcName}({funcNamedParams})", "{funcReturn}");
     dump_html_{funcReturn}(result, settings, 0);
@@ -1170,7 +1172,7 @@ std::ostream& dump_html_{funcName}(ApiDumpInstance& dump_inst, {funcTypedParams}
 
     settings.stream() << "<div class='thd'>Thread " << dump_inst.threadID() << ":</div>";
     if(settings.showTimestamp())
-        settings.stream() << "<div class='time'>Time: " << dump_inst.current_time_since_start().count() << " μs</div>";
+        settings.stream() << "<div class='time'>Time: " << dump_inst.current_time_since_start().count() << " us</div>";
     settings.stream() << "<details class='fn'><summary>";
     dump_html_nametype(settings.stream(), settings.showType(), "{funcName}({funcNamedParams})", "{funcReturn}");
     settings.stream() << "</summary>";
@@ -1581,8 +1583,9 @@ std::ostream& dump_json_{funcName}(ApiDumpInstance& dump_inst, {funcTypedParams}
     settings.stream() << settings.indentation(3) << "\\\"thread\\\" : \\\"Thread " << dump_inst.threadID() << "\\\",\\n";
 
     // Display elapsed time
-    if(settings.showTimestamp())
-    settings.stream() << settings.indentation(3) << "\\\"time\\\" : \\\""<< dump_inst.current_time_since_start().count() << " μs\\\",\\n";
+    if(settings.showTimestamp()) {{
+        settings.stream() << settings.indentation(3) << "\\\"time\\\" : \\\""<< dump_inst.current_time_since_start().count() << " us\\\",\\n";
+    }}
 
     // Display return value
     settings.stream() << settings.indentation(3) << "\\\"returnType\\\" : " << "\\\"{funcReturn}\\\",\\n";
