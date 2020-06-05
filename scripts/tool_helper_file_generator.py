7582067dae92fb8f250472ba9878835e81ae2d61
Lionel Landwerlin <lionel.g.landwerlin@intel.com>
2020-06-05 12:45:27 +0300

    Fix generator scripts
    Using positional arguments instead of named ones leads to desynchro
    between the Vulkan-Headers repo and this one. Since all arguments are
    named, better use names and avoid breakage.

--- scripts/tool_helper_file_generator.py
+++ scripts/tool_helper_file_generator.py
@@ -54,9 +54,19 @@ class ToolHelperFileOutputGeneratorOptions(GeneratorOptions):
                  alignFuncParam = 0,
                  library_name = '',
                  helper_file_type = ''):
-        GeneratorOptions.__init__(self, conventions, filename, directory, apiname, profile,
-                                  versions, emitversions, defaultExtensions,
-                                  addExtensions, removeExtensions, emitExtensions, sortProcedure)
+        GeneratorOptions.__init__(self,
+                                  conventions=conventions,
+                                  filename=filename,
+                                  directory=directory,
+                                  apiname=apiname,
+                                  profile=profile,
+                                  versions=versions,
+                                  emitversions=emitversions,
+                                  defaultExtensions=defaultExtensions,
+                                  addExtensions=addExtensions,
+                                  removeExtensions=removeExtensions,
+                                  emitExtensions=emitExtensions,
+                                  sortProcedure=sortProcedure)
         self.prefixText       = prefixText
         self.genFuncPointers  = genFuncPointers
         self.protectFile      = protectFile
