2d76404ed68921389703953612cd9487e2c5b26e
Richard Wright <richard@lunarg.com>
2020-06-30 14:40:30 -0700

    vkconfig2: Help fixes

--- vkconfig/dlghelp.cpp
+++ vkconfig/dlghelp.cpp
@@ -1,6 +1,9 @@
 #include "dlghelp.h"
 #include "ui_dlghelp.h"
 
-dlgHelp::dlgHelp(QWidget *parent) : QWidget(parent), ui(new Ui::dlgHelp) { ui->setupUi(this); }
+dlgHelp::dlgHelp(QWidget *parent) : QWidget(parent), ui(new Ui::dlgHelp) {
+    ui->setupUi(this);
+    setWindowFlags(Qt::WindowStaysOnTopHint);
+}
 
 dlgHelp::~dlgHelp() { delete ui; }
