aa280e02abcbfc1aa5b74b2fd0867b4ebdd7fab6
christophe-lunarg <christophe@lunarg.com>
2020-09-15 15:22:22 +0200

    vkconfig: Fix configuration renaming #1144
    Change-Id: Icb210fde1e04372e28c48cd12a87e26de15cbc2c

---
+++ vkconfig/alert.cpp
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2020 Valve Corporation
+ * Copyright (c) 2020 LunarG, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors:
+ * - Christophe Riccio <christophe@lunarg.com>
+ */
+
+#include "alert.h"
+
+#include <QMessageBox>
+
+void Alert::ConfigurationRenamingFailed() {
+    QMessageBox alert;
+    alert.setWindowTitle("Renaming of the layers configuration failed...");
+    alert.setText("There is already a configuration with the same name.");
+    alert.setInformativeText("Use a different name for the configuration.");
+    alert.setStandardButtons(QMessageBox::Ok);
+    alert.setDefaultButton(QMessageBox::Ok);
+    alert.setIcon(QMessageBox::Warning);
+    alert.exec();
+}
+
+void Alert::ConfigurationNameEmpty() {
+    QMessageBox alert;
+    alert.setWindowTitle("Renaming of the layers configuration failed...");
+    alert.setText("The configuration name is empty.");
+    alert.setInformativeText("The configuration name is required.");
+    alert.setStandardButtons(QMessageBox::Ok);
+    alert.setDefaultButton(QMessageBox::Ok);
+    alert.setIcon(QMessageBox::Warning);
+    alert.exec();
+}
