838e15a52312070c59e7cae4710c6c90e4dbe605
Christophe <christophe@lunarg.com>
2020-12-11 15:44:50 +0100

    vkconfig: Surrender layers on crash
    - If the user interupt Vulkan Configurator (^C) or Vulkan Configurator crash
    surrender the layers to avoid the layers override to remain without the user
    knowning it.
    
    Change-Id: I977c297598ad199811c3d23c4c0b271e47a1aad7

---
+++ vkconfig/main_signal.cpp
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2020 Valve Corporation
+ * Copyright (c) 2020 LunarG, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Authors:
+ * - Christophe Riccio <christophe@lunarg.com>
+ */
+
+#include "../vkconfig_core/override.h"
+#include "../vkconfig_core/layer_manager.h"
+
+#include "main_signal.h"
+
+#include <csignal>
+
+void SurrenderLayers(int signal) {
+    PathManager paths;
+    Environment environment(paths);
+
+    SurrenderLayers(environment);
+}
+
+void InitSignals() {
+    std::signal(SIGINT, SurrenderLayers);
+    std::signal(SIGTERM, SurrenderLayers);
+    std::signal(SIGSEGV, SurrenderLayers);
+    std::signal(SIGABRT, SurrenderLayers);
+    std::signal(SIGILL, SurrenderLayers);
+    std::signal(SIGFPE, SurrenderLayers);
+}
