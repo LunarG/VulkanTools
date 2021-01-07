20349453837c42c68ef19935c8b98eac2b594d9b
Christophe <christophe@lunarg.com>
2021-01-07 16:52:40 +0100

    vkconfig: Add JSON layer loader tests
    Change-Id: Ibb326cc1c5fce52309ce0df92c5a0063f5126363

--- vkconfig_core/test/test_layer.cpp
+++ vkconfig_core/test/test_layer.cpp
@@ -38,3 +38,79 @@ TEST(test_layer, collect_settings) {
     LayerSettingData* data0 = FindByKey(data, "key0");
     EXPECT_STREQ("value0", data0->value.c_str());
 }
+
+TEST(test_layer, load_v1_X_0_layer_header) {
+    static const char* SOURCES[] = {":/Layer 1.3.0.json", ":/Layer 1.4.0 - setting platforms.json"};
+
+    static const char* FILE_FORMAT_VERSIONS[] = {"1.3.0", "1.4.0"};
+
+    for (std::size_t i = 0, n = countof(SOURCES); i < n; ++i) {
+        Layer layer;
+        const bool load_loaded = layer.Load(SOURCES[i], LAYER_TYPE_EXPLICIT);
+        ASSERT_TRUE(load_loaded);
+
+        EXPECT_STREQ(FILE_FORMAT_VERSIONS[i], layer.file_format_version.str().c_str());
+
+        EXPECT_STREQ("VK_LAYER_LUNARG_test", layer.key.c_str());
+        EXPECT_STREQ("GLOBAL", layer._type.c_str());
+        EXPECT_STREQ(".\\VkLayer_test.dll", layer._library_path.c_str());
+        EXPECT_STREQ("1.2.162", layer._api_version.str().c_str());
+        EXPECT_STREQ("2", layer._implementation_version.c_str());
+        EXPECT_STREQ("test layer", layer.description.c_str());
+
+        EXPECT_EQ(0, layer.presets.size());
+    }
+}
+
+TEST(test_layer, load_v1_4_0_layer_setting_platform) {
+    Layer layer;
+    const bool load_loaded = layer.Load(":/Layer 1.4.0 - setting platforms.json", LAYER_TYPE_EXPLICIT);
+    ASSERT_TRUE(load_loaded);
+
+    EXPECT_EQ(PLATFORM_WINDOWS_BIT, FindByKey(layer.settings, "case0")->platform_flags);
+    EXPECT_EQ(PLATFORM_LINUX_BIT, FindByKey(layer.settings, "case1")->platform_flags);
+    EXPECT_EQ(PLATFORM_MACOS_BIT, FindByKey(layer.settings, "case2")->platform_flags);
+    EXPECT_EQ(PLATFORM_ALL_BIT, FindByKey(layer.settings, "case3")->platform_flags);
+    EXPECT_EQ(PLATFORM_ALL_BIT, FindByKey(layer.settings, "case4")->platform_flags);
+    EXPECT_EQ(PLATFORM_WINDOWS_BIT | PLATFORM_MACOS_BIT, FindByKey(layer.settings, "case5")->platform_flags);
+}
+
+TEST(test_layer, load_v1_4_0_layer_setting_default_value) {
+    Layer layer;
+    const bool load_loaded = layer.Load(":/Layer 1.4.0 - setting types.json", LAYER_TYPE_EXPLICIT);
+    ASSERT_TRUE(load_loaded);
+
+    EXPECT_EQ(SETTING_COUNT, layer.settings.size());
+
+    EXPECT_STREQ("value1", FindByKey(layer.settings, "enum")->default_value.c_str());
+    EXPECT_STREQ("flag0,flag1", FindByKey(layer.settings, "flags")->default_value.c_str());
+    EXPECT_STREQ("A string", FindByKey(layer.settings, "string")->default_value.c_str());
+    EXPECT_STREQ("TRUE", FindByKey(layer.settings, "bool")->default_value.c_str());
+    EXPECT_STREQ("1", FindByKey(layer.settings, "bool_numeric")->default_value.c_str());
+    EXPECT_STREQ("./test.txt", FindByKey(layer.settings, "save_file")->default_value.c_str());
+    EXPECT_STREQ("./test", FindByKey(layer.settings, "save_folder")->default_value.c_str());
+    EXPECT_STREQ("./test.txt", FindByKey(layer.settings, "load_file")->default_value.c_str());
+    EXPECT_STREQ("76", FindByKey(layer.settings, "int")->default_value.c_str());
+    EXPECT_STREQ("76-82", FindByKey(layer.settings, "range")->default_value.c_str());
+    EXPECT_STREQ("stringB,stringC", FindByKey(layer.settings, "message_id_filter")->default_value.c_str());
+}
+
+TEST(test_layer, load_v1_4_0_layer_preset) {
+    Layer layer;
+    const bool load_loaded = layer.Load(":/Layer 1.4.0 - presets.json", LAYER_TYPE_EXPLICIT);
+    ASSERT_TRUE(load_loaded);
+
+    EXPECT_EQ(2, layer.presets.size());
+
+    EXPECT_STREQ("Preset0", layer.presets[0].label.c_str());
+    EXPECT_STREQ("Description0", layer.presets[0].description.c_str());
+    EXPECT_EQ(PLATFORM_ALL_BIT, layer.presets[0].platform_flags);
+    EXPECT_EQ(STATUS_STABLE, layer.presets[0].status_type);
+    EXPECT_STREQ("A string0", FindByKey(layer.presets[0].settings, "stringA")->value.c_str());
+
+    EXPECT_STREQ("Preset1", layer.presets[1].label.c_str());
+    EXPECT_STREQ("Description1", layer.presets[1].description.c_str());
+    EXPECT_EQ(PLATFORM_WINDOWS_BIT, layer.presets[1].platform_flags);
+    EXPECT_EQ(STATUS_BETA, layer.presets[1].status_type);
+    EXPECT_STREQ("A string1", FindByKey(layer.presets[1].settings, "stringA")->value.c_str());
+}
