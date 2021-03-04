63d89532e330a8e4ccbde2539e75ee1f05594df0
Bob Ellison <bob@lunarg.com>
2021-03-03 17:03:25 -0700

    changes during live review - incomplete

--- tests/devsim_layer_tests.py
+++ tests/devsim_layer_tests.py
@@ -17,41 +17,53 @@
 """
 Some objects and functions to run tests for the VK_LAYER_LUNARG_device_simulation layer.
 This script will be modified in the future when LunarG/VulkanTools is updated to use the
-CI test harness written by Bob Ellison.
+CI test harness written by Bob Ellison.  It is intended to be usable by VulkanTools
+developers in their own repository clones, as well as in external CI using GitHub Actions
+and internal CI using Jenkins.
+
+Only desktop Vulkan is currently supported; considerable redesign would be
+required to be able to do these exercises on Android.
 """
 
 import argparse
 import json
+import logging
 import os
 import subprocess
 import sys
 
+log = logging.getLogger(__name__)
 
-# Set to True when debugging.
-VERBOSE = False
+def standalone_run(command_list):
+    """This utility is used for external execution of binaries.
+    It is encapsulated here and passed as a parameter to the
+    higher-level execution functions because a caller (e.g.
+    internal CI) may prefer to use their own execution function.
+    This function always returns the generated stdout and stderr
+    of the executed process so the caller can examine them."""
+    p = subprocess.run(command_list, check=True, capture_output=True)
+    return p.stdout, p.stderr
 
-def compare_primitive_value(v_1, v_2, debug_indent=""):
-    """
-    A basic compare function for primitive types such as numbers or strings.
-    """
-    if VERBOSE:
-        print("{}Comparing primitive values {} and {}".format(debug_indent, v_1, v_2))
-    if type(v_1) is type(v_2):
-        return v_1 == v_2
-
-    return False
-
+# Our tests will have vulkaninfo output with some expected values and
+# a lot of values that we don't care about (and that are sensitive to the test
+# environment, like the SDK version or the graphics driver version), e.g.
+# a list of all extensions... When we compare our generated output with
+# expected output, we really just want to make sure the values we need
+# are present, and ignore everything else.
+#
+# We could implement this with a custom dict class that alters the
+# comparison operator.  But for now we're implementing our own recursive
+# dictionary comparisons and list comparisons (because we likely have lists
+# of dictionaries).
 
 def compare_lists(list_1, list_2, debug_indent=""):
     """
     A custom method for comparing lists.
     """
-    if VERBOSE:
-        print("{}Comparing lists...".format(debug_indent))
+    log.debug('%sComparing lists...', debug_indent)
 
     if len(list_1) != len(list_2):
-        if VERBOSE:
-            print("{}Lists unequal lengths!".format(debug_indent))
+        log.debug("%sLists unequal lengths! %d vs %d", debug_indent, len(list_1), len(list_2))
         return False
     for value_1, value_2 in zip(list_1, list_2):
         if not check_types_and_compare_values(value_1,
@@ -68,14 +80,13 @@ def is_dict_subset(dict_1, dict_2, debug_indent=""):
     Dictionaries do not have to be completely equal.
     dict_1 simply needs to be a subset contained within dict_2.
     """
-    if VERBOSE:
-        print("{}Comparing dictionaries...".format(debug_indent))
+    log.debug("%sComparing dictionaries...", debug_indent)
 
     for k in dict_1:
         try:
             if not check_types_and_compare_values(dict_1[k],
-                                          dict_2[k],
-                                          debug_indent + "\t"):
+                                                  dict_2[k],
+                                                  debug_indent + "\t"):
                 return False
         except KeyError:
             return False
@@ -94,37 +105,83 @@ def check_types_and_compare_values(struct_1, struct_2, debug_indent=""):
     if isinstance(struct_1, dict) and isinstance(struct_2, dict):
         return is_dict_subset(struct_1, struct_2, debug_indent)
 
-    if isinstance(struct_1, struct_2.__class__) or isinstance(struct_2, struct_1.__class__):
-        return compare_primitive_value(struct_1, struct_2, debug_indent + "\t")
+    if struct_1 != struct_2:
+        log.debug("%sPrimitive comparison of %s and %s failed!", debug_indent, struct_1, struct_2)
+        return False
 
-    if VERBOSE:
-        err = "{}Comparing structures of different types: {} vs {}"
-        print(err.format(debug_indent,
-                         type(struct_1).__name__,
-                         type(struct_2).__name__))
-    return False
+    return True
 
+class DevsimLayerTestException(Exception):
+    """The exception class for this module."""
 
 class BaseTest:
     """
     A class that implements and defines some base functions and a base initializer for devsim tests.
+    The devsim_input_files are a list of filenames; if they're relative, they're relative to
+    the same directory where this script resides.  Absolute paths may also be provided.
+    The expected_vulkaninfo_output can be a dictionary containing subset vulkaninfo
+    output to compare against, or can specify a relative or absolute filename that contains
+    a more extensive set of vulkaninfo output.
+
+    This test requires that a valid VK_LAYER_PATH and (for Linux) LD_LIBRARY_PATH
+    are already set up in the enviroment.
     """
-    def __init__(self, test_environment, output_filename, answer_filename):
-        self.test_environment = test_environment
-        self.output_filepath = os.path.join(os.getcwd(), output_filename)
+    def __init__(self,
+                 devsim_input_files,
+                 expected_vulkaninfo_output,
+                 vulkaninfo_path,
+                 vulkaninfo_args=None,
+                 run=standalone_run,
+                 # These variables specify the environment variables needed
+                 # for a particular test case.
+                 instance_layers="VK_LAYER_LUNARG_device_simulation",
+                 debug_enable=0,
+                 exit_on_error=0,
+                 filename="",
+                 portability=0,
+                 extension_list=0):
 
-        if answer_filename is not None:
-            with open(os.path.join(os.getcwd(), answer_filename)) as answer_file:
-                self.answers = json.load(answer_file)
+        self.vulkaninfo_path = vulkaninfo_path
+        self.run = run
+
+        # Create the list of absolute paths we'll need for VK_DEVSIM_FILENAME.
+        devsim_absolute_files = []
+        for devsim_file in devsim_input_files:
+            full_path = os.path.abspath(devsim_file)
+            devsim_absolute_files.append(full_path)
+            if not os.path.isfile(full_path):
+                log.debug('devsim input file %s (normalized %s) does not exist',
+                          devsim_file, full_path)
+        log.debug("devsim absolute files: %s", devsim_absolute_files)
+
+        # This dictionary of environment values will be added to the run environment
+        # for this test case.
+        self.test_environment = {
+            "VK_INSTANCE_LAYERS": str(instance_layers),
+            "VK_DEVSIM_DEBUG_ENABLE": str(debug_enable),
+            "VK_DEVSIM_EXIT_ON_ERROR": str(exit_on_error),
+            "VK_DEVSIM_FILENAME": os.pathsep.join(devsim_absolute_files),
+            "VK_DEVSIM_EMULATE_PORTABILITY_SUBSET_EXTENSION": str(portability),
+            "VK_DEVSIM_MODIFY_EXTENSION_LIST": str(extension_list),
+        }
+
+        # Get the expected subset dictionary output for use later.
+        if isinstance(expected_vulkaninfo_output, dict):
+            self.expected_vulkaninfo_output = expected_vulkaninfo_output
         else:
-            self.answers = {}
+            output_file = os.path.abspath(expected_vulkaninfo_output)
+            if not os.path.isfile(output_file):
+                raise DevsimLayerTestException("expected output file does not exist: {}".format(output_file))
 
-    def run(self, vulkaninfo_path):
-        """
-        Base function to run the test.
-        Implemented by classes that inherit from BaseTest.
-        """
-        raise NotImplementedError()
+            with open(output_file) as f:
+                self.expected_vulkaninfo_output = json.load(f)
+
+    def check_output(self, stdout):
+        """Tests that need to also directly examine the vulkaninfo output
+        will override this function to do their additional checks.  By
+        default this function doesn't do any additional checking, making
+        it appropriate for most of the tests."""
+        return True
 
     def check(self):
         """
@@ -132,10 +189,16 @@ class BaseTest:
         Compares output of the "run" function to an "answer" file.
         If the run output contains the values in the answer file, the test is considered passed.
         """
-        with open(self.output_filepath) as output_file:
-            test_results = json.load(output_file)
-            return check_types_and_compare_values(self.answers, test_results)
-
+        command_list = [self.vulkaninfo_path]
+        if self.vulkaninfo_args is not None:
+            command_list.extend(self.vulkaninfo_args)
+        stdout, stderr = self.run(command_list)
+        actual_vulkaninfo_output = json.loads(stdout)
+        return (
+            check_types_and_compare_values(self.expected_vulkaninfo_output,
+                                           actual_vulkaninfo_output) and
+            self.check(stdout)
+        )
 
 class BasicTest(BaseTest):
     """
@@ -350,6 +413,9 @@ def main():
     """
     Main function to run the basic tests.
     """
+
+    logging.basicConfig(level=logging.INFO)
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "vulkan_tools_install_dir",
