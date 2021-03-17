61e7f35afdafdd5a5a2278b48647698d33fa3a18
Christophe <christophe@lunarg.com>
2021-03-17 18:45:23 +0100

    vkconfig: Fixed unit test build
    Change-Id: Ice3ef3659d53e189603d51006408d77aac671c06

--- vkconfig_core/setting_meta.cpp
+++ vkconfig_core/setting_meta.cpp
@@ -104,7 +104,7 @@ bool SettingMetaList::Equal(const SettingMeta& other) const {
 
     if (this->list != meta.list) return false;
 
-    return this->default_values == meta.default_values;
+    return this->default_value == meta.default_value;
 }
 
 bool operator==(const SettingEnumValue& a, const SettingEnumValue& b) {
