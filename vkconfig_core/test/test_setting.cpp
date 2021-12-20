956481aa80e671f0fc30e619d685423ffc1fb668
Christophe <christophe@lunarg.com>
2021-12-20 16:52:41 +0100

    vkconfig: remove dead code

--- vkconfig_core/test/test_setting.cpp
+++ vkconfig_core/test/test_setting.cpp
@@ -32,12 +32,3 @@ TEST(test_setting, get_setting_token) { EXPECT_STREQ("STRING", GetToken(SETTING_
 
 TEST(test_setting, get_setting_type) { EXPECT_EQ(SETTING_STRING, GetSettingType("STRING")); }
 
-TEST(test_setting, get_setting_save_file) { EXPECT_TRUE(IsPath(SETTING_SAVE_FILE)); }
-
-TEST(test_setting, get_setting_load_file) { EXPECT_TRUE(IsPath(SETTING_LOAD_FILE)); }
-
-TEST(test_setting, get_setting_save_folder) { EXPECT_TRUE(IsPath(SETTING_SAVE_FOLDER)); }
-
-TEST(test_setting, get_setting_string) { EXPECT_TRUE(!IsPath(SETTING_STRING)); }
-
-TEST(test_setting, get_setting_bool) { EXPECT_TRUE(!IsPath(SETTING_BOOL)); }
