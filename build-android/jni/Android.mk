6aa73403bfe6377b760be073db0059c80127b06a
Christophe <christophe@lunarg.com>
2022-03-24 16:02:49 +0100

    devsim: remove Android build script
    Change-Id: Ibf746dc257a8e5fc4b1ae1ed531493d9a294205c

--- build-android/jni/Android.mk
+++ build-android/jni/Android.mk
@@ -71,22 +71,5 @@ LOCAL_CPPFLAGS += -DVK_ENABLE_BETA_EXTENSIONS -DVK_USE_PLATFORM_ANDROID_KHR -DVK
 LOCAL_LDLIBS    := -llog
 include $(BUILD_SHARED_LIBRARY)
 
-include $(CLEAR_VARS)
-LOCAL_MODULE := VkLayer_device_simulation
-LOCAL_SRC_FILES += $(SRC_DIR)/layersvt/device_simulation.cpp
-LOCAL_SRC_FILES += $(SRC_DIR)/layersvt/vk_layer_table.cpp
-LOCAL_SRC_FILES += $(ANDROID_DIR)/third_party/jsoncpp/dist/jsoncpp.cpp
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/$(ANDROID_DIR)/third_party/jsoncpp/dist \
-                    $(LOCAL_PATH)/$(THIRD_PARTY)/Vulkan-Headers/include \
-                    $(LOCAL_PATH)/$(LVL_DIR)/layers \
-                    $(LOCAL_PATH)/$(LVL_DIR)/layers/generated \
-                    $(LOCAL_PATH)/$(SRC_DIR)/layersvt \
-                    $(LOCAL_PATH)/$(LAYER_DIR)/include
-LOCAL_STATIC_LIBRARIES += layer_utils
-LOCAL_CPPFLAGS += -std=c++11 -Wall -Werror -Wno-unused-function -Wno-unused-const-variable -mxgot
-LOCAL_CPPFLAGS += -DVK_ENABLE_BETA_EXTENSIONS -DVK_USE_PLATFORM_ANDROID_KHR -fexceptions
-LOCAL_LDLIBS    := -llog
-include $(BUILD_SHARED_LIBRARY)
-
 $(call import-module,android/native_app_glue)
 $(call import-module,third_party/googletest)
