165e499a73a1721ab681e26cc972700eed5976ff
Christophe <christophe@lunarg.com>
2022-11-21 17:53:36 +0100

    vkconfig: improve handling of robust oob warn
    Change-Id: Ifd74cd529d9ab289b878381bcdf55aca6fb05637

--- vkconfig/settings_tree.cpp
+++ vkconfig/settings_tree.cpp
@@ -297,6 +297,7 @@ static bool IsBuiltinValidationSetting(const Parameter &parameter, const std::st
     keys.push_back("printf_verbose");
     keys.push_back("printf_buffer_size");
     keys.push_back("gpuav_buffer_oob");
+    keys.push_back("warn_on_robust_oob");
     keys.push_back("validate_draw_indirect");
     keys.push_back("vma_linear_output");
     keys.push_back("fine_grained_locking");
