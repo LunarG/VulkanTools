ee965aa3ac457593b232d4672faee574d2efd4ad
Charles Giessen <charles@lunarg.com>
2023-09-15 11:18:27 -0600

    layers: Update to new names in vk_dispatch_table.h
    The functions and structs in vk_dispatch_table.h have been updated to use a
    'vku' prefix instead of 'vul'. Thus, this repo needs to update to it so the
    next header update goes smoothly.

--- layersvt/vk_layer_table.h
+++ layersvt/vk_layer_table.h
@@ -25,24 +25,24 @@
 #include <unordered_map>
 #include <cstring>
 
-typedef std::unordered_map<void *, VulDeviceDispatchTable *> device_table_map;
-typedef std::unordered_map<void *, VulInstanceDispatchTable *> instance_table_map;
-VulDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa, device_table_map &map);
-VulDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa);
-VulInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa, instance_table_map &map);
-VulInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa);
+typedef std::unordered_map<void *, VkuDeviceDispatchTable *> device_table_map;
+typedef std::unordered_map<void *, VkuInstanceDispatchTable *> instance_table_map;
+VkuDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa, device_table_map &map);
+VkuDeviceDispatchTable *initDeviceTable(VkDevice device, const PFN_vkGetDeviceProcAddr gpa);
+VkuInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa, instance_table_map &map);
+VkuInstanceDispatchTable *initInstanceTable(VkInstance instance, const PFN_vkGetInstanceProcAddr gpa);
 
 typedef void *dispatch_key;
 
 dispatch_key get_dispatch_key(const void *object);
 
-VulDeviceDispatchTable *device_dispatch_table(void *object);
+VkuDeviceDispatchTable *device_dispatch_table(void *object);
 
-VulInstanceDispatchTable *instance_dispatch_table(void *object);
+VkuInstanceDispatchTable *instance_dispatch_table(void *object);
 
-VulDeviceDispatchTable *get_dispatch_table(device_table_map &map, void *object);
+VkuDeviceDispatchTable *get_dispatch_table(device_table_map &map, void *object);
 
-VulInstanceDispatchTable *get_dispatch_table(instance_table_map &map, void *object);
+VkuInstanceDispatchTable *get_dispatch_table(instance_table_map &map, void *object);
 
 VkLayerInstanceCreateInfo *get_chain_info(const VkInstanceCreateInfo *pCreateInfo, VkLayerFunction func);
 VkLayerDeviceCreateInfo *get_chain_info(const VkDeviceCreateInfo *pCreateInfo, VkLayerFunction func);
