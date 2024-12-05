5f343d803c27f3bb6d61740e5312fb57cbbfcab3
Christophe <christophe@lunarg.com>
2024-12-05 20:35:24 +0100

    vkconfig3: Fix corner cases config UI enabling

--- vkconfig_gui/settings_tree.cpp
+++ vkconfig_gui/settings_tree.cpp
@@ -83,7 +83,8 @@ void SettingsTreeManager::CreateGUI() {
 
     this->ui->configurations_group_box_settings->blockSignals(true);
     this->ui->configurations_group_box_settings->setTitle(format("%s:", title.c_str()).c_str());
-    this->ui->configurations_group_box_settings->setCheckable(configurator.HasEnabledUI(ENABLE_UI_SETTINGS));
+    this->ui->configurations_group_box_settings->setEnabled(configurator.HasEnabledUI(ENABLE_UI_SETTINGS));
+    this->ui->configurations_group_box_settings->setCheckable(true);
     this->ui->configurations_group_box_settings->setChecked(parameter->override_settings);
     this->ui->configurations_group_box_settings->blockSignals(false);
     this->ui->configurations_presets->setVisible(!layer->presets.empty());
